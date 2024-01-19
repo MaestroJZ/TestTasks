5ca2b24ccdf72d445dcad89550af584cdbfc46c7
MaestroJZ <eldar_02_03@outlook.com>
2024-01-19 15:23:13 +0600

    Сделал калькулятор

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double result;
+        Console.Write("a = ");
+        if (double.TryParse(Console.ReadLine(), out double a))
+        {
+            Console.Write("b = ");
+            if (double.TryParse(Console.ReadLine(), out double b))
+            {
+                Console.Write("Операция: ");
+                if (char.TryParse(Console.ReadLine(), out char o))
+                {
+                    switch (o)
+                    {
+                        case '+':
+                            result = a + b;
+                            Console.Write($"{a} + {b} = {result}");
+                            break;
+                        case '-':
+                            result = a - b;
+                            Console.Write($"{a} - {b} = {result}");
+                            break;
+                        case '*':
+                            result = a * b;
+                            Console.Write($"{a} * {b} = {result}");
+                            break;
+                        case '/':
+                            if (b == 0) { Console.Write("На 0 нельзя делить"); }
+                            else
+                            {
+                                result = a / b;
+                                Console.Write($"Результат: {result}");
+                            }
+                            break;
+                        default:
+                            Console.Write("Неправильно ввели операцию");
+                            break;
+                    }
+                }
+                else { Console.Write("Ошибка при вводе операций"); }
+            }
+            else { Console.Write("Ошибка при вводе b"); }
+        }
+        else { Console.Write("Ошибка при вводе a"); }
+    }
+}
